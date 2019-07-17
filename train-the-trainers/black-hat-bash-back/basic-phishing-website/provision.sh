74be2148b45d09dec7630046a7843a9b0566126d
Meitar M <meitarm@gmail.com>
2019-07-17 15:11:31 -0400

    Better instructions in the Vagrant post-up message, fix perms in guest.

--- train-the-trainers/black-hat-bash-back/basic-phishing-website/provision.sh
+++ train-the-trainers/black-hat-bash-back/basic-phishing-website/provision.sh
@@ -2,6 +2,7 @@
 
 apt update
 apt install -y python python-pip xfce4 firefox
-git clone --depth 1 https://github.com/trustedsec/social-engineer-toolkit/ set/
+git clone --depth 1 https://github.com/trustedsec/social-engineer-toolkit.git set/
+chown -R vagrant:vagrant set
 cd set
 pip install -r requirements.txt
