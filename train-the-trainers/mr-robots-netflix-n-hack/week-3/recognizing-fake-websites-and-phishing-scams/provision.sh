a031ab698cb5adbb1fbe52cb8490e61a275149f2
Meitar M <meitarm@gmail.com>
2019-12-10 00:58:55 -0500

    Use fork ahead of Trusted Sec's master branch to fix pymssql issue.

--- train-the-trainers/mr-robots-netflix-n-hack/week-3/recognizing-fake-websites-and-phishing-scams/provision.sh
+++ train-the-trainers/mr-robots-netflix-n-hack/week-3/recognizing-fake-websites-and-phishing-scams/provision.sh
@@ -2,7 +2,7 @@
 
 apt update
 apt install --yes python python-pip xfce4 firefox
-git clone --depth 1 https://github.com/trustedsec/social-engineer-toolkit.git set/
+git clone --depth 1 --branch fix-pymssql --single-branch https://github.com/meitar/social-engineer-toolkit.git set/
 chown -R vagrant:vagrant set
 cd set
 pip install -r requirements.txt
