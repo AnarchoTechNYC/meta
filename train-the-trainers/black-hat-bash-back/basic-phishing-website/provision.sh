8a73886ecc678c0d251773a2b5eb180eb310457f
Meitar M <meitarm@gmail.com>
2020-03-04 16:04:05 -0500

    Copy commit b71ddf6aa8b5980bdc7d49ff3ec51d47054fa471 until labs diverge.

--- train-the-trainers/black-hat-bash-back/basic-phishing-website/provision.sh
+++ train-the-trainers/black-hat-bash-back/basic-phishing-website/provision.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -
 
 apt update
-apt install --yes python python-pip xfce4 firefox
+apt install --yes python3 python3-pip xfce4 firefox
 git clone --depth 1 https://github.com/trustedsec/social-engineer-toolkit.git set/
 chown -R vagrant:vagrant set
 cd set
-pip install -r requirements.txt
+pip3 install -r requirements.txt
