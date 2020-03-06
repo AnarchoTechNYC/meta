7e5a90d47b4eadbfde24a7144cb47bd0d740fa70
Meitar M <meitarm@gmail.com>
2020-03-05 20:37:00 -0500

    Add recent mitmproxy to the "Basic Phishing Site" lab environment.

--- train-the-trainers/mr-robots-netflix-n-hack/week-3/recognizing-fake-websites-and-phishing-scams/provision.sh
+++ train-the-trainers/mr-robots-netflix-n-hack/week-3/recognizing-fake-websites-and-phishing-scams/provision.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -
 
 apt update
-apt install --yes python3 python3-pip xfce4 firefox
+apt install --yes python3 python3-pip python3-venv xfce4 firefox
+
+# Install Social Engineer's Toolkit.
 git clone --depth 1 https://github.com/trustedsec/social-engineer-toolkit.git set/
 chown -R vagrant:vagrant set
 cd set
 pip3 install -r requirements.txt
+
+# Install recent mitmproxy, as well.
+pip3 install pipx
+sudo --login -u vagrant pipx install mitmproxy
