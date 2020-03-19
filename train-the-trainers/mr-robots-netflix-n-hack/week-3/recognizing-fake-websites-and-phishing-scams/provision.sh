da4deccddfebd29f3e7011e1eff2da72cac5954a
Meitar M <meitarm@gmail.com>
2020-03-19 13:51:54 -0400

    Remove Black Hat Bash Back phishing lab now that Mr. Robot's is fuller.

--- train-the-trainers/mr-robots-netflix-n-hack/week-3/recognizing-fake-websites-and-phishing-scams/provision.sh
+++ train-the-trainers/mr-robots-netflix-n-hack/week-3/recognizing-fake-websites-and-phishing-scams/provision.sh
@@ -1,16 +1,35 @@
 #!/bin/bash -
+#
+# This script provisions the demo and lab environment. It installs the
+# following tools:
+#
+#     * Ettercap
+#     * Social Engineer's Toolkit (SET)
+#     * mitmproxy
+#     * Evilginx
+#
+# In addition, it installs an XFCE4 Desktop Environment along with the
+# Firefox Web browser, and a few necessary utilities. The additional
+# attack tools are included to support various sections of this lab's
+# "Discussion" sections.
 
 apt update
-apt install --yes python3 python3-pip python3-venv \
+apt install --yes python3 python3-pip python3-venv unzip \
     xfce4 firefox \
     ettercap-graphical
 
-# Install Social Engineer's Toolkit.
 git clone --depth 1 https://github.com/trustedsec/social-engineer-toolkit.git set/
 chown -R vagrant:vagrant set
 cd set
 pip3 install -r requirements.txt
 
-# Install recent mitmproxy, as well.
 pip3 install pipx
 sudo --login -u vagrant pipx install mitmproxy
+
+mkdir /tmp/evilginx2
+wget --quiet -O /tmp/evilginx2/evilginx2.zip \
+    https://github.com/kgretzky/evilginx2/releases/download/2.3.0/evilginx_linux_x86_2.3.0.zip
+unzip -d /tmp/evilginx2 /tmp/evilginx2/evilginx2.zip
+# Execute in external `bash` process as install script not executable.
+bash /tmp/evilginx2/install.sh && chmod a+x /usr/local/bin/evilginx
+rm -rf /tmp/evilginx2
