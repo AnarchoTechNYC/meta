beb3755a004dfda4ba9bc44125750f98085db523
fabacab <fabacab@riseup.net>
2020-05-25 23:16:01 -0400

    Initial skeleton for new practice lab, update wiki submodule.

---
+++ train-the-trainers/practice-labs/network-perimeter-defenses/provision/ubuntu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+apt update && apt install --yes nftables \
+    lightdm xfce4 xfce4-terminal firefox gufw
+
+# PolicyKit looks in the `sudo` group for privileged users,
+# so add `vagrant` user to that group so we can run `gufw`.
+usermod -a -G sudo vagrant
+
+# LightDM expects GNOME, not Xfce, by default. Override.
+cat <<EOF > /etc/lightdm/lightdm.conf.d/50-xfce-vagrant.conf
+[Seat:*]
+user-session=xfce
+autologin-user=vagrant
+autologin-user-timeout=0
+EOF
+systemctl start lightdm.service
