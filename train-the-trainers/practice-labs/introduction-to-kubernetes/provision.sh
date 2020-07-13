7c6772acdbc7ab7511ed5bd39d74ce2797cecc4c
fabacab <fabacab@riseup.net>
2020-07-12 22:38:59 -0400

    Clone example k8s configurations, better idempotence, post-up message.

--- train-the-trainers/practice-labs/introduction-to-kubernetes/provision.sh
+++ train-the-trainers/practice-labs/introduction-to-kubernetes/provision.sh
@@ -18,9 +18,15 @@ snap install --classic kubectl \
     && kubectl completion bash > /etc/bash_completion.d/kubectl
 
 # Install Minikube.
-curl --silent --location --output minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
-    && chmod +x minikube
-install minikube /usr/local/bin/ && rm minikube
+which minikube || {
+    curl --silent --location --output minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+    chmod +x minikube
+    install minikube /usr/local/bin/
+    rm minikube
+}
 
 # Start the Minikube cluster as the default non-root (`vagrant`) user.
 sudo --login -u vagrant minikube start 2> /tmp/minikube-start.log || cat /tmp/minikube-start.log
+
+# Download some workshop materials.
+[ -d k8s-workshop ] || sudo --login -u vagrant git clone https://github.com/fabacab/k8s-workshop.git
